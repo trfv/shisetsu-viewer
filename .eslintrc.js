8cae942cbfa1df707207023a348bda912e784de8
trfv <trfv471955@gmail.com>
2020-12-16 22:27:13 +0900

    minor fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,51 +1,44 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "es6": true
+  env: {
+    browser: true,
+    es6: true,
   },
-  "extends": [
-    "plugin:prettier/recommended",
-    "prettier/@typescript-eslint",
-    "prettier/react"
-  ],
-  "plugins": [
-    "react",
-    "@typescript-eslint"
-  ],
-  "globals": {
-    "Atomics": "readonly",
-    "SharedArrayBuffer": "readonly"
+  extends: ["plugin:prettier/recommended", "prettier/@typescript-eslint", "prettier/react"],
+  plugins: ["react", "@typescript-eslint"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
   },
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "ecmaFeatures": {
-      "jsx": true
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "ecmaVersion": 2018,
-    "sourceType": "module"
+    ecmaVersion: 2018,
+    sourceType: "module",
   },
-  "rules": {
-    "semi": ["error", "always"],
+  rules: {
+    semi: ["error", "always"],
     "prettier/prettier": [
       "error",
       {
-        "printWidth": 100,
-        "tabWidth": 2,
-        "singleQuote": false,
-        "trailingComma": "es5",
-      }
+        printWidth: 100,
+        tabWidth: 2,
+        singleQuote: false,
+        trailingComma: "es5",
+      },
     ],
     "@typescript-eslint/camelcase": "off",
     "no-restricted-imports": [
       "error",
       {
-        "patterns": ["@material-ui/*/*/*", "!@material-ui/core/test-utils/*"]
-      }
-    ]
+        patterns: ["@material-ui/*/*/*", "!@material-ui/core/test-utils/*"],
+      },
+    ],
   },
-  "settings": {
-    "react": {
-      "version": "detect",
+  settings: {
+    react: {
+      version: "detect",
     },
-  }
+  },
 };
