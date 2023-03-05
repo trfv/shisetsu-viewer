42adf2e0522fa12822fb717a1604d09336ff9101
trfv <35182743+trfv@users.noreply.github.com>
2023-03-06 00:47:53 +0900

    some updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,19 @@
-import { ConfigEnv, defineConfig, splitVendorChunkPlugin } from "vite";
+import { defineConfig, splitVendorChunkPlugin } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }: ConfigEnv) => ({
+export default defineConfig(({ mode }) => ({
   build: {
     sourcemap: mode !== "production",
     outDir: "build",
     rollupOptions: {
+      external: [
+        "/fonts/roboto-v27-latin-regular.woff2",
+        "/fonts/roboto-v27-latin-700.woff2",
+        "/fonts/noto-sans-jp-v28-latin_japanese-regular.woff2",
+        "/fonts/noto-sans-jp-v28-latin_japanese-700.woff2",
+      ],
       output: {
         esModule: true,
         manualChunks: {
