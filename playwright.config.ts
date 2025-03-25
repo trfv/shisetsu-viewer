0a187be207607f3eaed00653f3966fc2b842c9cd
trfv <35182743+trfv@users.noreply.github.com>
2025-03-25 11:46:37 +0900

    update workflow and timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,6 @@ import { defineConfig } from "@playwright/test";
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  timeout: process.env.CI ? 60 * 60 * 1000 : 5 * 60 * 1000,
   tsconfig: "./tsconfig.json",
   /* Directory to search for tests */
   testDir: "./packages/scraper",
@@ -21,17 +20,19 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 3 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
+  /* timeout setting. See https://playwright.dev/docs/test-timeouts */
+  timeout: process.env.CI ? 60 * 60 * 1000 : 15 * 60 * 1000,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    actionTimeout: 10 * 1000,
+    navigationTimeout: 30 * 1000,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
+    trace: process.env.CI ? "off" : "on-first-retry",
   },
 });
