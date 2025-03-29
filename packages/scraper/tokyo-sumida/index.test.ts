2fde8e75f6c1b7f4066783a78135806200b4543e
trfv <35182743+trfv@users.noreply.github.com>
2025-03-30 00:26:08 +0900

    お試し

--- packages/scraper/tokyo-sumida/index.test.ts
+++ packages/scraper/tokyo-sumida/index.test.ts
@@ -16,6 +16,12 @@ const facilityNames = [
   "みどりコミュニティセンター",
 ];
 
+test.use({
+  locale: "ja-JP",
+  userAgent:
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36",
+});
+
 facilityNames.forEach((name) => {
   test(name, async ({ page }) => {
     console.time(name);
--- packages/scraper/tokyo-sumida/index.ts
+++ packages/scraper/tokyo-sumida/index.ts
@@ -62,7 +62,13 @@ function buildISODateStrings(headerStartDate: string, dates: string[]): string[]
 }
 
 export async function prepare(page: Page, facilityName: string): Promise<Page> {
-  await page.goto("https://yoyaku03.city.sumida.lg.jp/user/Home");
+  try {
+    await page.goto("https://yoyaku03.city.sumida.lg.jp/user/Home", { timeout: 10 * 60 * 1000 });
+  } catch (error) {
+    console.error(`Failed to navigate to the page. url: ${page.url()}`);
+    page.screenshot({ path: `error_${Date.now()}.png` });
+    throw error;
+  }
   await page.getByRole("tab", { name: "利用目的から探す" }).click();
   await page.getByText("器楽演奏", { exact: true }).click();
   await page.getByText("器楽演奏（現地相談）", { exact: true }).click();
