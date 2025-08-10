954750a077fc01acd3485ca6b5440cabe197c62a
trfv <35182743+trfv@users.noreply.github.com>
2025-08-10 11:28:10 +0900

    bump up storybook

--- packages/viewer/components/Button/Button.stories.tsx
+++ packages/viewer/components/Button/Button.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from "@storybook/react";
+import type { Meta, StoryObj } from "@storybook/react-vite";
 import { BaseButton as Button } from "./BaseButton";
 
 export default {
--- packages/viewer/components/CheckboxGroup/CheckboxGroup.stories.tsx
+++ packages/viewer/components/CheckboxGroup/CheckboxGroup.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from "@storybook/react";
+import type { Meta, StoryObj } from "@storybook/react-vite";
 import type { ChangeEvent } from "react";
 import { useState } from "react";
 import { Checkbox } from "../Checkbox";
