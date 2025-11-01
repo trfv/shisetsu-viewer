689ec487d772e4f92078956d92635492595572e4
trfv <35182743+trfv@users.noreply.github.com>
2025-11-01 23:22:16 +0900

    update config

--- packages/viewer/vitest.config.ts
+++ packages/viewer/vitest.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
 import { playwright } from "@vitest/browser-playwright";
 
